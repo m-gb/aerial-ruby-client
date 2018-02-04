d3a6dba90e6d1504de597ba7646551e2eee299f8
m-gb <maya.goldberg90@gmail.com>
2018-02-04 22:08:27 +0100

    restructure

--- aerial_ruby_client.gemspec
+++ aerial_ruby_client.gemspec
@@ -13,14 +13,16 @@ Gem::Specification.new do |spec|
   spec.description           = "A simple gem that downloads the Aerial Apple TV videos and saves them to a given directory."
   spec.homepage              = "http://rubygems.org/gems/aerial_ruby_client"
   spec.license               = "MIT"
-
-  spec.files                 = ["lib/aerial_ruby_client.rb"]
-  spec.files                 = ["lib/xscreensaver_script.rb"]
+  
+  spec.files                 = `git ls-files -z`.split("\x0").reject do |f|
+    f.match(%r{^(test|spec|features)/})
+  end
+  spec.require_paths         = ["lib"]
 
   spec.required_ruby_version = '>= 2.4.0'
 
-  spec.add_dependency "bundler",  "~> 1.16"
-  spec.add_dependency "rake",     "~> 10.0"
+  spec.add_development_dependency "bundler",  "~> 1.16"
+  spec.add_development_dependency "rake",     "~> 10.0"
   spec.add_dependency "httparty", "~> 0.15.6"
   spec.add_dependency "json",     "~> 2.1"
 end
--- lib/aerial_ruby_client.rb
+++ lib/aerial_ruby_client.rb
@@ -1,39 +1,42 @@
 #!/usr/bin/env ruby
 
+require 'aerial_ruby_client/version'
+
 require 'httparty'
 require 'json'
 require 'open-uri'
 require 'fileutils'
 
-def download_aerials(foldername)
-  aerialdir = File.join(Dir.home, foldername)
-  #=> /home/username/foldername
+module AerialRubyClient
+  def download_aerials(foldername)
+    aerialdir = File.join(Dir.home, foldername)
+    #=> /home/username/foldername
+  
+    Dir.mkdir(aerialdir) unless Dir.exists?(aerialdir)
 
-  Dir.mkdir(aerialdir) unless Dir.exists?(aerialdir)
-
-  url = 'http://a1.phobos.apple.com/us/r1000/000/Features/atv/' +
-        'AutumnResources/videos/entries.json'
-
-  response = HTTParty.get(url).to_s
-  parsed = JSON.parse(response)
-  parsed.each do |item|
-    item['assets'].each do |asset|
-      filename = asset['timeOfDay'] + '-' + asset['id'] + '.mov'
-      filepath = File.join(aerialdir, filename)
-      if File.exists?(filepath)
-        puts "File #{filename} already exists, skipping..."
-      else
-        puts "Downloading #{filename} to #{aerialdir}"
-        open(asset['url']) do |u|
-          File.open(filepath, 'wb') { |f| f.write(u.read) }
+    url = 'http://a1.phobos.apple.com/us/r1000/000/Features/atv/' +
+          'AutumnResources/videos/entries.json'
+  
+    response = HTTParty.get(url).to_s
+    parsed = JSON.parse(response)
+    parsed.each do |item|
+      item['assets'].each do |asset|
+        filename = asset['timeOfDay'] + '-' + asset['id'] + '.mov'
+        filepath = File.join(aerialdir, filename)
+        if File.exists?(filepath)
+          puts "File #{filename} already exists, skipping..."
+        else
+          puts "Downloading #{filename} to #{aerialdir}"
+          open(asset['url']) do |u|
+            File.open(filepath, 'wb') { |f| f.write(u.read) }
+          end
         end
       end
     end
-  end
+    
+    puts 'Finished processing all URLs!'
   
-  puts 'Finished processing all URLs!'
-
-  # Copies the xscreensaver script to the target folder.
-  FileUtils.cp("#{__dir__}/xscreensaver_script.rb", aerialdir)
-
+    # Copies the xscreensaver script to the target folder.
+    FileUtils.cp("#{__dir__}/aerial_ruby_client/xscreensaver_script.rb", aerialdir)
+  end
 end
